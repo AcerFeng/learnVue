25e26892032ee54dee8b69f7e4bb2775c86c6bc4
AcerFeng <lanfeng@lanfengdeMac-mini.local>
2017-04-27 14:14:52 +0800

    解决冲突

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -6,8 +6,18 @@
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      totalcount: 200,
+      content: '',
+    };
+  },
+  computed: {
+    reduceCount() {
+      return this.totalcount - this.content.length;
+    },
+  },
+};
 </script>
 
 <style lang="less">
