9defe4349f002ecd2b5339647c3c83ccd858ee1b
AcerFeng <qinlanfeng@foxmail.com>
2017-05-05 02:03:47 +0800

    vue-router 初试

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -6,20 +6,9 @@
 
 <script>
 export default {
-  data() {
-    return {
-      totalcount: 200,
-      content: '',
-    };
-  },
-  computed: {
-    reduceCount() {
-      return this.totalcount - this.content.length;
-    },
-  },
 };
 </script>
 
-<style lang="less">
+<style>
 
 </style>
